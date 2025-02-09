f9fc1d5a8bf79140c4989e38d0fa75ca298b929a
Sam Radinfar <hhesamradinfar@gmail.com>
2025-02-09 11:32:42 +0100

    Ajouter des filters avec js et CSS

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -24,14 +24,49 @@ async function getWorks() {
 getWorks();
 
 
-
-
 function setFigure(data){
 	const figure = document.createElement("figure");
 	figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
 				        <figcaption>${data.title}</figcaption>`;
 
 	document.querySelector(".gallery").append(figure);
+}
+
+
+
+
+
+async function getCategories() {
+
+	const url = "http://localhost:5678/api/categories";
+	
+	try {
+
+	  const response = await fetch(url);
+
+	  if (!response.ok) {
+		throw new Error(`Response status: ${response.status}`);
+	  }
+	  const json = await response.json();
+	  console.log(json);
+
+	  for(let i = 0; i < json.length; i++){
+		setFilter(json[i]);
+	  }
+
+	}
+	catch (error) {
+	  console.error(error.message);
+	}
+}
+getCategories();
+
+
+function setFilter(data){
+	const div = document.createElement("div");
+	div.innerHTML = `${data.name}`;
+
+	document.querySelector(".div-projets").append(div);
 }
 
 
